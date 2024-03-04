16b77c73f22aa3db719fd26d158de30991136363
Jasmine Lee <jasminelee_0409@hotmail.com>
2024-03-03 20:24:20 -0500

    Added another dependency, import files, and added more components(empty)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import Footer from './components/Footer';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
 
 function App() {
 
   return (
     <>
-
+    
     </>
   )
 }
