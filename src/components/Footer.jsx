bbe4a4cec4609fd006cac8b29cecc1c0f5dd2232
Jasmine Lee <jasminelee_0409@hotmail.com>
2024-03-08 20:25:38 -0500

    Added Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+    return (
+      <div>
+        <a href='https://github.com/Jasmineleeyt' target='_blank' rel='noopener'>
+            <img className='footer-icon' src='../../public/github-logo.png' alt='Github icon' width="30" height="30"/>
+        </a>
+        <a href='https://ca.linkedin.com/' target='_blank' rel='noopener'>
+            <img className='footer-icon' src='../../public/linkedin-logo.png' alt='Linkedin icon' width="35" height="30"/>
+        </a>
+      </div>
+    );
+  }
