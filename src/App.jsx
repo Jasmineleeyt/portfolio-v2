c916190862eae6554abad7ea1e3a70fdb60fef93
Jasmine Lee <jasminelee_0409@hotmail.com>
2024-03-09 23:28:09 -0500

    Modified App.jsx to use Outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 // import './App.css'
 // import Header from './components/Header';
-// import Navigation from './components/Navigation';
-import Footer from './components/Footer';
+import Navigation from './components/Navigation';
+// import Footer from './components/Footer/Footer';
 // import About from './components/About';
 // import Portfolio from './components/Portfolio';
-import Contact from './components/Contact';
+// import Contact from './components/Contact/Contact';
 // import Resume from './components/Resume';
+// import Portfolio from './pages/Portfolio';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Contact />
-      <Footer />
+      <Navigation />
+      <Outlet/>
     </>
   );
 }
-
 
 export default App
