6c5b8d3f0bf0bedbd4d86a6f041f3977eef2bb8f
Jasmine Lee <jasminelee_0409@hotmail.com>
2024-03-10 13:53:37 -0400

    Updated App.jsx to render header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // import './App.css'
-// import Header from './components/Header';
-import Navigation from './components/Navigation';
-// import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+// import Navigation from './components/Navigation';
+import Footer from './components/Footer/Footer';
 // import About from './components/About';
 // import Portfolio from './components/Portfolio';
 // import Contact from './components/Contact/Contact';
@@ -12,8 +12,9 @@ import { Outlet } from 'react-router-dom';
 function App() {
   return (
     <>
-      <Navigation />
-      <Outlet/>
+      <Header />
+      <Outlet />
+      <Footer />
     </>
   );
 }
